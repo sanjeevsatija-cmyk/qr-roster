86132ce2b8454d3d96d200e8baea65feb0d3ac6d
Sanjeev <sanjeevsatija@gmail.com>
2026-05-16 11:51:15 +1000

    fix: revert base path back to GitHub Pages

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/qr-roster/sw.js', { scope: '/qr-roster/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
